50a919e51888ca89a72871c2217d01967e7f74ce
Dominique Dresen <dominique.dresen@uni-koeln.de>
2017-11-27 11:43:58 +0100

    Switched from Metamask to Infura for connecting to Ethereum Mainnet.

--- src/app/shared/web3-connect.service.ts
+++ src/app/shared/web3-connect.service.ts
@@ -1,43 +1,49 @@
-import { Injectable, Output, EventEmitter } from '@angular/core';
+import { Injectable } from '@angular/core';
 
 import Web3 from 'web3';
 
 @Injectable()
 export class Web3ConnectService {
 
-  @Output() update = new EventEmitter();
-  private web3Instance: any;
-  private nodeConnected: boolean = false;
+  private _web3: any;
+
   constructor() {}
 
   connectToNode(): void { 
-      if (typeof window['web3'] !== 'undefined') {
-        console.log('Connecting to Web3 using Metamask.');
-        this.web3 = new this.Web3(window['web3'].currentProvider);
-        console.log('Web3 version:', this.web3.version);
-        this.nodeConnected = true;
+      console.log('Connecting to web3.')
+      if (typeof this._web3 !== 'undefined') {
+        this.web3 = new this.Web3(this.web3.currentProvider);
       } else {
-        console.log('No web3 in your browser? You should consider trying MetaMask!');
-        this.web3 = new this.Web3(new this.Web3.providers.HttpProvider("http://localhost:8545"));
+        this.web3 = new this.Web3(new this.Web3.providers.HttpProvider('https://mainnet.infura.io/506w9CbDQR8fULSDR7H0'));
       }
-  }
-
-  get isConnected(): boolean {
-    return this.nodeConnected;
+      console.log('Web3 version:', this.web3.version);
   }
 
   get web3(): any {
-    if (!this.web3Instance) {
+    if (!this._web3) {
         this.connectToNode();
     }
-    return this.web3Instance;
+    return this._web3;
   }
 
   set web3(web3: any) {
-    this.web3Instance = web3;
+    this._web3 = web3;
   }
  
   get Web3(): any {
       return Web3;
   }
 }
+
+
+
+      // connect to Metamask
+      // if (typeof window['web3'] !== 'undefined') {
+      //   console.log('Connecting to Web3 using Metamask.');
+      //   this.web3 = new this.Web3(window['web3'].currentProvider);
+      //   console.log('Web3 version:', this.web3.version);
+      //   this.nodeConnected = true;
+      // } else {
+      //   console.log('No web3 in your browser? You should consider trying MetaMask!');
+      //   this.web3 = new this.Web3(new this.Web3.providers.HttpProvider("http://localhost:8545"));
+      // }
