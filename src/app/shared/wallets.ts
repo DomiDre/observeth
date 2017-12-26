65e121be4e9c31ca884c4b6cbe65a7773cdb1b79
Dominique Dresen <dominique.dresen@uni-koeln.de>
2017-12-26 23:42:46 +0100

    Adding name for wallets

---
+++ src/app/shared/wallets.ts
@@ -0,0 +1,5 @@
+export var EtherWallets = {
+  '0x3f5CE5FBFe3E9af3971dD833D26bA9b5C936f0bE': 'Binance',
+  '0x8d12A197cB00D4747a1fe03395095ce2A5CC6819': 'etherdelta_2',
+  '0x5E575279bf9f4acf0A130c186861454247394C06': 'liqui.io_2',
+}
